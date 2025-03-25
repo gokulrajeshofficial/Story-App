fcd712dc121582e06107c44f3f7bdbf446b13ff6
Gokul Rajesh <gokulrajesh23@gmail.com>
2025-03-26 05:18:50 +0530

    [fix] : bug fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -28,7 +28,9 @@ axios.interceptors.response.use(
   (error) => {
     if (error.response?.status === 401) {
       localStorage.removeItem('token');
-      window.location.href = '/login';
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login';
+      }
     }
     return Promise.reject(error);
   }
